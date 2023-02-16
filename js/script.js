ea42a40627104098cb3b4832a9f630ac80079153
Vasyl Derenko <v.derenko@5328-OC.local>
2023-02-16 15:21:29 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,31 +1,77 @@
-const slider = (active, inActive) => {
-  const onboardings = document.querySelectorAll(inActive);
+new Blobity({ color: "rgb(255, 255, 255)", fontSize: 14, zIndex: -1 });
 
-  const button = document.querySelectorAll(`${inActive} .next`);
-  button.forEach((current) => {
-    current.addEventListener("click", () => {
-      const index = current.getAttribute("data-next-index");
-      onboardings.forEach((current) => current.classList.remove(active));
-      document
-        .querySelector(`${inActive}[data-index="${index}"]`)
-        .classList.add(active);
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  TagCanvas.Start("skills", "", {
+    textColour: "#fff",
+    textHeight: 20,
+    depth: 0.99,
+    zoom: 1,
+    zoomMax: 1,
+    zoomMin: 1,
+    outlineColour: "#08fdd8",
+    initial: [0.3, -0.1],
   });
-};
-
-slider("transition--visible", ".transition");
-slider("transition-mobile--visible", ".transition-mobile");
-
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-window.addEventListener("resize", () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
-$(document).ready(function () {
-  $("#fullpage").fullpage();
+const burger = document.querySelector(".hamburger");
+const burgerMenu = document.querySelector(".burger-menu");
+const range = document.querySelector(".range");
+const rangeThumb = range.querySelector("span");
+const body = document.querySelector("body");
+const links = document.querySelectorAll(".burger-menu nav a");
+
+const toggleNavigation = () => {
+  burgerMenu.classList.toggle("open");
+  body.classList.toggle("active");
+  burger.classList.toggle("is-active");
+};
+
+burger.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  toggleNavigation();
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute("href"));
+    target.scrollIntoView({
+      behavior: "smooth",
+    });
+
+    toggleNavigation();
+  });
+});
+
+const thumbWidth =
+  range.clientWidth / document.querySelectorAll(".portfolio__block").length;
+
+rangeThumb.style.width = `${thumbWidth}px`;
+let slidesLength;
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  navigation: {
+    nextEl: ".arrow-next",
+    prevEl: ".arrow-back",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+swiper.on("slideChange", (swiper) => {
+  const { realIndex } = swiper;
+
+  rangeThumb.style.left = `${thumbWidth * realIndex}px`;
 });
